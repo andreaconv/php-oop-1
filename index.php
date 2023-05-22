48caccc5ba50b8559251a610cfd8e105512525ae
andreaconv <andrea2convertini@gmail.com>
2023-05-22 22:41:07 +0200

    [add] array di film

--- index.php
+++ index.php
@@ -2,15 +2,16 @@
 
 require_once 'models/Movie.php';
 
-$babyDriver = new Movie('Baby Driver', 'Azione', 115, new Dettagli(2017, '3.4/5'));
-$theMask = new Movie('The Mask', 'Commedia', 100, new Dettagli(1994, '4.3/5'));
+$movies = [
+  new Movie('Baby Driver', 'Azione', 115, new Dettagli(2017, '3.4')),
+  new Movie('The Mask', 'Commedia', 100, new Dettagli(1994, '4.3')),
+  new Movie('Il gigante di ferro', 'Avventura ', 86, new Dettagli(1999, '3.2')),
+];
 
-var_dump($babyDriver);
-var_dump($theMask);
 
+var_dump($movies);
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">

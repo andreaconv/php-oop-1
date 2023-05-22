93d672e3d7767bd35f18a0418d96317db449328d
andreaconv <andrea2convertini@gmail.com>
2023-05-22 22:19:30 +0200

    [add] metodo

--- index.php
+++ index.php
@@ -3,18 +3,35 @@
 class Movie{
   public $titolo;
   public $genere;
+  public $lunghezza;
 
-  function __construct($_titolo, $_genere){
+  function __construct($_titolo, $_genere, $_lunghezza){
     $this->titolo = $_titolo;
     $this->genere = $_genere;
+    $this->lunghezza = $this->getTime($_lunghezza);
+  }
+
+  public function getTime($_lunghezza){
+
+    // return number_format($_lunghezza/60, 2, '.', "") . ' ore';
+    // return new DateTime()->format('%H:')
+
+
+    $zero    = new DateTime('@0');
+    $offset  = new DateTime('@' . $_lunghezza * 60);
+    $diff    = $zero->diff($offset);
+    return $diff->format('%hh, %Im');
+
+
   }
 }
 
-$babyDriver = new Movie('Baby Driver', 'Azione');
-$theMask = new Movie('The Mask', 'Commedia');
+$babyDriver = new Movie('Baby Driver', 'Azione', 115);
+$theMask = new Movie('The Mask', 'Commedia', 100);
 
 var_dump($babyDriver);
 var_dump($theMask);
+
 
 ?>
 
